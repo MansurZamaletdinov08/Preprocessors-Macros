b38f3ffb266748468ffd67d785e60643615dbdc1
Mansur Zamaletdinov <zamaletdinov98@list.ru>
2022-12-18 22:47:40 +0300

    NewKode

---
+++ PreprocDirectives/PreprocDirectives.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#define MODE 1
+
+#ifndef MODE
+#error Error
+#endif
+
+#if defined MODE == 0
+std::cout << "Работаю в режиме тренировки";
+#endif
+
+#if MODE == 1
+//std::cout << "Работаю в боевом режиме.";
+#define add(type, a, b) ((a) + (b))
+
+#else
+std::cout << "Неизвестный режим. Завершение работы";
+#endif
+
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	int a, b;
+	std::cout << "Введите число 1: ";
+	std::cin >> a;
+
+	std::cout << "Введите число 2: ";
+	std::cin >> b;
+
+	std::cout << add(int, a, b);
+}
