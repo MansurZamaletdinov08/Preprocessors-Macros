7bea1517a2fcd7ad7fd9c3d480942c069b40b19c
Mansur Zamaletdinov <101476332+MansurZamaletdinov08@users.noreply.github.com>
2022-12-20 20:08:14 +0300

    Update PreprocDirectives.cpp

--- PreprocDirectives/PreprocDirectives.cpp
+++ PreprocDirectives/PreprocDirectives.cpp
@@ -1,31 +1,35 @@
-﻿#include <iostream>
+#include <iostream>
 #define MODE 1
 
 #ifndef MODE
 #error Error
 #endif
 
-#if defined MODE == 0
-std::cout << "Работаю в режиме тренировки";
-#endif
 
 #if MODE == 1
-std::cout << "Работаю в боевом режиме.";
 #define add(type, a, b) ((a) + (b))
-
-#else MODE
-std::cout << "Неизвестный режим. Завершение работы";
 #endif
+
 
 
 int main() {
 	setlocale(LC_ALL, "Ru");
 	int a, b;
+	#if MODE == 1
+	std::cout << "Работаю в боевом режиме." << std::endl;
 	std::cout << "Введите число 1: ";
 	std::cin >> a;
 
 	std::cout << "Введите число 2: ";
 	std::cin >> b;
 
-	std::cout << add(int, a, b);
+	std::cout << "Результат сложения: " << add(int, a, b) << std::endl;
+
+
+	#elif MODE == 0
+	std::cout << "Работаю в режиме тренировки";
+
+	#else MODE
+	std::cout << "Неизвестный режим. Завершение работы";
+	#endif
 }
